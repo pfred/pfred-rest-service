828b35ea2905bd1b6856f180b9e9554c12e3e404
Dario <cruz.da@husky.neu.edu>
2019-12-02 14:08:05 -0500

    Adding macaque and chimp into the list of indexes for species

--- scripts/pfred/bowtie2.pm
+++ scripts/pfred/bowtie2.pm
@@ -12,34 +12,40 @@ my $BOWTIE_BUILD="$ENV{BOWTIE_BUILD}";
 
 #my ($BOWTIE,$BOWTIE_BUILD,$BOWTIE_INDEXES)= & exportEnv();
 
-my %AntisenseIndex=("human" => ["HumancDNA.v56", "allHumanUnsplicedGene.v55"],
-			"mouse"=>["MousecDNA.v55","allMouseUnsplicedGene.v55"],
-			"rat"=>["RatcDNA.v55","allRatUnsplicedGene.v55"],
-			);
-			
-my %siRNAIndex=("human" => ["HumancDNA.v56"],
-                        "mouse"=>["MousecDNA.v55"],
-                        "rat"=>["RatcDNA.v55"],
-                        );
+my %AntisenseIndex=("human" => ["allHumancDNA.v55", "allHumanUnsplicedGene.v55"],
+                    "mouse"=>["allMousecDNA.v55","allMouseUnsplicedGene.v55"],
+                    "rat"=>["allRatcDNA.v55","allRatUnsplicedGene.v55"],
+                    "chimp"=>["allChimpcDNA.v55", "allChimpUnsplicedGene.v55"],
+                    "dog"=>["allDogcDNA.v55", "allDogUnsplicedGene.v55"],
+                    "macaca"=>["allMacacacDNA.v55", "allMacacaUnsplicedGene.v55"]
+    );
+
+my %siRNAIndex=("human" => ["allHumancDNA.v55"],
+                "mouse"=>["allMousecDNA.v55"],
+                "rat"=>["allRatcDNA.v55"],
+                "chimp"=>["allChimpcDNA.v55"],
+                "dog"=>["allDogcDNA.v55"],
+                "macaca"=>["allMacacacDNA.v55"]
+    );
 
 sub exportEnv
 {
-	print STDERR "ENV: $BOWTIE\n";
-        print STDERR "*** ['$BOWTIE', '$BOWTIE_INDEXES', '$BOWTIE_BUILD'] ***\n";
-	return ($BOWTIE,$BOWTIE_INDEXES,$BOWTIE_BUILD);
+    print STDERR "ENV: $BOWTIE\n";
+    print STDERR "*** ['$BOWTIE', '$BOWTIE_INDEXES', '$BOWTIE_BUILD'] ***\n";
+    return ($BOWTIE,$BOWTIE_INDEXES,$BOWTIE_BUILD);
 }
 
 sub exportAntisenseIndexes
 {
-	my ($species)=@_;
-	#my $indexlist=join ",", @{$AntisenseIndex{"$species"}};
-	print STDERR "AntisenseIndexList: @{$AntisenseIndex{$species}}\n";
-	return (@{$AntisenseIndex{"$species"}});
+    my ($species)=@_;
+    #my $indexlist=join ",", @{$AntisenseIndex{"$species"}};
+    print STDERR "AntisenseIndexList: @{$AntisenseIndex{$species}}\n";
+    return (@{$AntisenseIndex{"$species"}});
 }
 
 sub exportsiRNAIndexes
 {
-	my ($species)=@_;
-	print STDERR "siRNAIndex: @{$siRNAIndex{$species}}\n";
-	return (@{$siRNAIndex{"$species"}});
+    my ($species)=@_;
+    print STDERR "siRNAIndex: @{$siRNAIndex{$species}}\n";
+    return (@{$siRNAIndex{"$species"}});
 }
