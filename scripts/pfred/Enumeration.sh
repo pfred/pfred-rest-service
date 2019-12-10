0906394a7ae0bcd1b41be8ba9b2e1d2a2f421d92
Dario <cruz.da@husky.neu.edu>
2019-12-10 13:11:27 -0500

    Fixing file merging bug

--- scripts/pfred/Enumeration.sh
+++ scripts/pfred/Enumeration.sh
@@ -7,6 +7,6 @@ RunDesign.py callBowtieEnumerate -p "$2" -l "$3" -f sequence.fa -o oligoOut.csv
 RunDesign.py joinOligoOut -l oligoOut.csv -j exonBoundaries.csv -v variationData.csv -o outputSummary_.csv
 
 cat outputSummary_.csv | awk -F"," '{print ","$1}' | sed -e 's/name/oligoName/g'> name.txt
-paste outputSummary_.csv name.txt > EnumerationResult.csv
+paste -d'\0' outputSummary_.csv name.txt > EnumerationResult.csv
 
 
